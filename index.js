507b3d73c50d74e403d93b92c872a4b2db55dfd3
Oketch <oketchngwani@gmail.com>
2024-12-15 12:27:50 +0900

    arithmatic -lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+function multiplyNumbers (num1, num2){
+let result =num1 * num2;
+return result
+}
+console.log(multiplyNumbers (2,31));
+let random = Math.floor(Math.random() * 10) + 1;
+
+console.log(random);
+function modNum (num3,num4){
+       return (num3%num4)
+}
+console.log(modNum(28,6));
+
+let max = Math.max(10, 5, 20, 8, 15);
+
+console.log(max); 
